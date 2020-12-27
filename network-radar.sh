498f618092f254b9467186732e058f5a935babfd
Gabriel Rufino <contato@gabrielrufino.com>
2020-12-27 15:33:11 -0300

    Update shell script using environment variables

--- network-radar.sh
+++ network-radar.sh
@@ -1,6 +1,13 @@
-cd /home/pi/Desktop/network-radar-api
-/usr/bin/nohup /home/pi/.nvm/versions/node/v14.15.3/bin/npm start </dev/null &>/dev/null &
-cd /home/pi/Desktop/network-radar-job
-/usr/bin/nohup /home/pi/.nvm/versions/node/v10.23.0/bin/npm start </dev/null &>/dev/null &
-cd /home/pi/Desktop/network-radar-app
-/usr/bin/nohup /home/pi/.nvm/versions/node/v14.15.3/bin/http-server build/ </dev/null &>/dev/null &
+API=/home/pi/Desktop/network-radar-api
+APP=/home/pi/Desktop/network-radar-app
+HTTP_SERVER=/home/pi/.nvm/versions/node/v14.15.3/bin/http-server
+JOB=/home/pi/Desktop/network-radar-job
+NOHUP=/usr/bin/nohup
+NPM=/home/pi/.nvm/versions/node/v10.23.0/bin/npm
+
+cd $API
+$NOHUP $NPM start </dev/null &>/dev/null &
+cd $JOB
+$NOHUP $NPM start </dev/null &>/dev/null &
+cd $APP
+$NOHUP $HTTP_SERVER build/ </dev/null &>/dev/null &
